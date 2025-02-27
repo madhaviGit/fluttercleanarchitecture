4c17f3065f8ff62dc98ec3f4a8173d8d0789a7e7
Madhavi <madhavi.gullipalli@intive.com>
2025-02-27 10:00:26 +0100

    add lint rules to app

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/cupertino.dart';
 
 import 'comman_main.dart';
 import 'flavor_config.dart';
 
-
-void main(){
+void main() {
   // runApp(const App(flavor:'Development'));
   WidgetsFlutterBinding.ensureInitialized();
   AppEnvironment.setUpEnv(Environmemt.stage);
