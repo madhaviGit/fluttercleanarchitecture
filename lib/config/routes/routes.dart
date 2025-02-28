1d54677638f1c9b0266ee1b20f1128def058cb77
Madhavi <madhavi.gullipalli@intive.com>
2025-02-28 11:24:18 +0100

    clean code

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../presentation/screens/HomeScreen.dart';
+import '../../presentation/screens/home_screen.dart';
 
 class AppRoutes {
   static Route onGenerateRoutes(RouteSettings settings) {
