ea788a46262055ba60263bba5dee687e0cd4f740
Madhavi <madhavi.gullipalli@intive.com>
2025-02-28 11:21:43 +0100

    remove unnecessary code

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,6 +1,7 @@
+import 'package:get_it/get_it.dart';
+
 import '../services/api_service.dart';
 import '../services/auth_service.dart';
-import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
 
