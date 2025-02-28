f06ce2c71a7dd5603a6be719f0e3c1c10c4c8de7
Madhavi <madhavi.gullipalli@intive.com>
2025-02-28 16:24:03 +0100

    Add Google Signin authentication

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 
 import '../services/api_service.dart';
 import '../services/auth_service.dart';
+import '../services/google_auth_service.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -9,4 +10,6 @@ void setupLocator() {
   // Register services
   getIt.registerSingleton<ApiService>(ApiService());
   getIt.registerSingleton<AuthService>(AuthService());
+  // Register GoogleAuthService as a lazy singleton
+  getIt.registerLazySingleton(GoogleAuthService.new);
 }
